9d481e23419997d9bd944539bf62f687c86b5baa
Guilherme Altobeli Crespo <51871518+TheQuinn1303@users.noreply.github.com>
2024-08-12 13:44:41 -0300

    Curso Asimov Desafio Locadora de Carros
    v.01

---
+++ main.py
@@ -0,0 +1,12 @@
+import os 
+os.system('cls')
+
+print('''
+    ====================
+    Locadoras de Carros 
+    ====================
+
+Oque Deseja Fazer? 
+0 - Mostrar Portifólio | 1 - Alugue  um Carro | 2 - Devolver um Carro 
+
+''')
